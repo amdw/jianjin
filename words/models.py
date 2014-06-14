b629fbbf9f411fcbfa3da8eea6a236f03ef5096e
Andrew Medworth <github@medworth.org.uk>
2014-06-14 19:00:49 +0800

    Use Django user model

--- words/models.py
+++ words/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-
-from django.db import models
 from django.core import urlresolvers
+from django.contrib.auth.models import User
 
 class Tag(models.Model):
     tag = models.CharField(max_length=20)
@@ -13,7 +12,7 @@ class Word(models.Model):
     word = models.CharField(max_length=10)
     pinyin = models.CharField(max_length=50)
     notes = models.TextField(blank=True)
-    user_id = models.CharField(max_length=20, blank=True)
+    user = models.ForeignKey(User)
     tags = models.ManyToManyField(Tag, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
