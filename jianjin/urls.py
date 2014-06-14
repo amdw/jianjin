58bdd7cb333a37f70b7f15b5990609e5eeb1419c
Andrew Medworth <github@medworth.org.uk>
2014-06-14 23:05:39 +0800

    Serve sensible HTML from root URL

--- jianjin/urls.py
+++ jianjin/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name="main.html")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^words/', include('words.urls')),
 )
