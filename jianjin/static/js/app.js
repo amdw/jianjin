30f9026e01421033f4f4b8188bac8ba7aacf90e1
Andrew Medworth <github@medworth.org.uk>
2014-07-01 16:38:35 +0800

    Move About text into a separate static file and put basic Angular routing in place

--- jianjin/static/js/app.js
+++ jianjin/static/js/app.js
@@ -1,1 +1,11 @@
 var jianjinApp = angular.module('jianjinApp', ['ngRoute']);
+
+jianjinApp.config(function ($routeProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: '/static/about.html',
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+});
