3f64fe7d54d3b4754ca4cd1485d4f70caf831ee8
Andrew Medworth <github@medworth.org.uk>
2014-07-01 16:22:32 +0800

    Put skeleton static HTML in place

--- jianjin/urls.py
+++ jianjin/urls.py
@@ -4,6 +4,7 @@ from django.views.generic.base import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
+# TODO: Is there a way to make the root URL point to a static file, instead of using a no-op template view?
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="main.html")),
     url(r'^admin/', include(admin.site.urls)),
