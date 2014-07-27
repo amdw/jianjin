a9e672a2a7725c156330f3c1a21cdba77c25eb7b
Andrew Medworth <github@medworth.org.uk>
2014-07-27 15:11:40 +0800

    Restructure Pinyin code and expose as filter

--- jianjin/static/js/pinyin.js
+++ jianjin/static/js/pinyin.js
@@ -1,58 +1,58 @@
 var pinyin = angular.module('pinyin', []);
 
-pinyin.constant('convert', function(text) {
-  var splitText = function(text) {
-    var result = [];
-    var i = 0;
-    while (true) {
-      if (i >= text.length) {
-        break;
-      }
-      var part = [];
-      while (i < text.length) {
-        part.push(text[i]);
-        if (text[i].match(/[0-9]/)) {
-          result.push(part.join(""));
-          part = [];
-        }
-        i += 1;
-      }
-      if (part.length > 0) {
+pinyin.constant('splitText', function(text) {
+  var result = [];
+  var i = 0;
+  while (true) {
+    if (i >= text.length) {
+      break;
+    }
+    var part = [];
+    while (i < text.length) {
+      part.push(text[i]);
+      if (text[i].match(/[0-9]/)) {
         result.push(part.join(""));
+        part = [];
       }
+      i += 1;
     }
-    return result;
-  };
-
-  var pickVowel = function(syllable) {
-    var firstVowelIdx = -1;
-    for (i = 0; i < syllable.length; i++) {
-      if (syllable[i].match('[aeouvAEOUV]')) {
-        return i;
-      }
-      if (syllable[i].match('[aeiouvAEIOUV]') && firstVowelIdx < 0) {
-	firstVowelIdx = i;
-      }
+    if (part.length > 0) {
+      result.push(part.join(""));
     }
-    return firstVowelIdx;
   }
+  return result;
+});
 
-  var vowels = {
-    'a': ['ā', 'á', 'ǎ', 'à'],
-    'A': ['Ā', 'Á', 'Ǎ', 'À'],
-    'e': ['ē', 'é', 'ě', 'è'],
-    'E': ['Ē', 'É', 'Ě', 'È'],
-    'i': ['ī', 'í', 'ǐ', 'ì'],
-    'I': ['Ī', 'Í', 'Ǐ', 'Ì'],
-    'o': ['ō', 'ó', 'ǒ', 'ò'],
-    'O': ['Ō', 'Ó', 'Ǒ', 'Ò'],
-    'u': ['ū', 'ú', 'ǔ', 'ù'],
-    'U': ['Ū', 'Ú', 'Ǔ', 'Ù'],
-    'v': ['ǖ', 'ǘ', 'ǚ', 'ǜ'],
-    'V': ['Ǖ', 'Ǘ', 'Ǚ', 'Ǜ']
-  };
+pinyin.constant('pickVowel', function(syllable) {
+  var firstVowelIdx = -1;
+  for (i = 0; i < syllable.length; i++) {
+    if (syllable[i].match('[aeouvAEOUV]')) {
+      return i;
+    }
+    if (syllable[i].match('[aeiouvAEIOUV]') && firstVowelIdx < 0) {
+	firstVowelIdx = i;
+    }
+  }
+  return firstVowelIdx;
+});
 
-  var convertPart = function(part) {
+pinyin.constant('vowels', {
+  'a': ['ā', 'á', 'ǎ', 'à'],
+  'A': ['Ā', 'Á', 'Ǎ', 'À'],
+  'e': ['ē', 'é', 'ě', 'è'],
+  'E': ['Ē', 'É', 'Ě', 'È'],
+  'i': ['ī', 'í', 'ǐ', 'ì'],
+  'I': ['Ī', 'Í', 'Ǐ', 'Ì'],
+  'o': ['ō', 'ó', 'ǒ', 'ò'],
+  'O': ['Ō', 'Ó', 'Ǒ', 'Ò'],
+  'u': ['ū', 'ú', 'ǔ', 'ù'],
+  'U': ['Ū', 'Ú', 'Ǔ', 'Ù'],
+  'v': ['ǖ', 'ǘ', 'ǚ', 'ǜ'],
+  'V': ['Ǖ', 'Ǘ', 'Ǚ', 'Ǜ']
+});
+
+pinyin.factory('convertPart', function(vowels, pickVowel) {
+  return function(part) {
     var syllableMatch = part.match('([a-zA-Z]+)([1-4])$');
     if (!syllableMatch) {
       return part;
@@ -64,11 +64,16 @@ pinyin.constant('convert', function(text) {
       return part;
     }
     var newVowel = vowels[syllable[vowelIdx]][tone-1];
-    return part.substr(0, part.length - syllable.length - 1) + syllable.substr(0, vowelIdx) + newVowel + syllable.substr(vowelIdx + 1);
-  };
+    return part.substr(0, part.length - syllable.length - 1) + 
+  	   syllable.substr(0, vowelIdx) + newVowel + syllable.substr(vowelIdx + 1);
+  }
+});
 
-  var parts = splitText(text);
-  var convertedParts = parts.map(convertPart);
-  return convertedParts.join("");
+pinyin.filter('pinyin', function(splitText, convertPart) {
+  return function(text) {
+    var parts = splitText(text);
+    var convertedParts = parts.map(convertPart);
+    return convertedParts.join("");
+  };
 });
 
--- jianjin/static/js/test/unit/pinyin.js
+++ jianjin/static/js/test/unit/pinyin.js
@@ -3,21 +3,21 @@
 describe('pinyin', function() {
   beforeEach(module('pinyin'));
 
-  it('should correctly convert single word', inject(function(convert) {
-    expect(convert("ni3hao3")).toBe("nǐhǎo");
-    expect(convert("bao3bei4")).toBe("bǎobèi");
-    expect(convert("Shuai4ge1")).toBe("Shùaigē");
-    expect(convert("CHUI1NIU2")).toBe("CHŪINIÚ");
-    expect(convert("mei3nv3")).toBe("měinǚ");
-    expect(convert("MEI3NV3")).toBe("MĚINǙ");
+  it('should correctly convert single word', inject(function(pinyinFilter) {
+    expect(pinyinFilter("ni3hao3")).toBe("nǐhǎo");
+    expect(pinyinFilter("bao3bei4")).toBe("bǎobèi");
+    expect(pinyinFilter("Shuai4ge1")).toBe("Shùaigē");
+    expect(pinyinFilter("CHUI1NIU2")).toBe("CHŪINIÚ");
+    expect(pinyinFilter("mei3nv3")).toBe("měinǚ");
+    expect(pinyinFilter("MEI3NV3")).toBe("MĚINǙ");
   }));
 
-  it('should correctly convert multiple words', inject(function(convert) {
-    expect(convert("ni3hao3 bao3bei4!")).toBe("nǐhǎo bǎobèi!");
+  it('should correctly convert multiple words', inject(function(pinyinFilter) {
+    expect(pinyinFilter("ni3hao3 bao3bei4!")).toBe("nǐhǎo bǎobèi!");
   }));
 
-  it('should ignore irrelevant text', inject(function(convert) {
-    expect(convert("nz3hao3")).toBe("nz3hǎo");
-    expect(convert("ni3hao5")).toBe("nǐhao5");
+  it('should ignore irrelevant text', inject(function(pinyinFilter) {
+    expect(pinyinFilter("nz3hao3")).toBe("nz3hǎo");
+    expect(pinyinFilter("ni3hao5")).toBe("nǐhao5");
   }));
 });
