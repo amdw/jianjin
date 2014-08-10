04279cfd86460b55bc838ae2570dd8ad79f12bfd
Andrew Medworth <github@medworth.org.uk>
2014-08-10 12:17:12 +0800

    Require login to access the app (#1)

--- jianjin/urls.py
+++ jianjin/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
 
 from django.contrib import admin
@@ -6,7 +7,8 @@ admin.autodiscover()
 
 # TODO: Is there a way to make the root URL point to a static file, instead of using a no-op template view?
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="main.html")),
+    url(r'^$', login_required(TemplateView.as_view(template_name="main.html"))),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     url(r'^words/', include('words.urls')),
 )
--- words/tests.py
+++ words/tests.py
@@ -11,12 +11,24 @@ class JsonApiTest(TestCase):
 
     def setUp(self):
         setup_test_environment()
+        self.client = Client()
+        self.assertTrue(self.client.login(username=USER, password=PASSWORD))
 
     def test_get_tags(self):
-        client = Client()
-        self.assertTrue(client.login(username=USER, password=PASSWORD))
-        response = client.get('/words/tags/')
+        response = self.client.get('/words/tags/')
         self.assertEqual(200, response.status_code)
         self.assertEqual('application/json', response['Content-Type'])
         json_response = json.loads(response.content)
         self.assertEqual(sorted(["awesome", "funny"]), sorted([t['tag'] for t in json_response]))
+
+class AuthenticationTest(TestCase):
+    fixtures = ['testdata.json']
+
+    def setUp(self):
+        setup_test_environment()
+
+    def test_html_redirect(self):
+        """Test that unauthenticated HTML requests are redirected to the login page"""
+        response = self.client.get('/', follow=True)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual([("http://testserver/accounts/login/?next=/", 302)], response.redirect_chain)
