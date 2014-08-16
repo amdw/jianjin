55ab86e275164016912ea039a0ca0c67b3a65312
Andrew Medworth <github@medworth.org.uk>
2014-08-16 11:55:57 +0800

    Add test which asserts JSON structure of a single word (#2)

--- words/tests.py
+++ words/tests.py
@@ -42,6 +42,28 @@ class JsonApiTest(TestCase):
         json_response = self.assert_successful_json(response)
         self.assertEqual(sorted([u"你好", u"蛋白质", u"乌龙球", u"妇女"]), sorted(w['word'] for w in json_response))
 
+    def test_get_word(self):
+        response = self.client.get('/words/words/1', follow=True)
+        word = self.assert_successful_json(response)
+        expected_word = {u'confidence': 10,
+                         u'date_added': u'2014-06-14T11:25:53.081Z',
+                         u'definitions': [{u'definition': u'Hello!',
+                                           u'example_sentences': [],
+                                           u'id': 1,
+                                           u'part_of_speech': u' ',
+                                           u'word': 1}],
+                         u'id': 1,
+                         u'last_modified': u'2014-06-14T14:29:15.857Z',
+                         u'notes': u'',
+                         u'pinyin': u'ni3hao3',
+                         u'related_words': [{u'id': 3,
+                                             u'pinyin': u'wu1long2qiu2',
+                                             u'word': u'\u4e4c\u9f99\u7403'}],
+                         u'tags': [u'awesome'],
+                         u'user': u'user',
+                         u'word': u'\u4f60\u597d'}
+        self.assertEqual(expected_word, word)
+
     def test_flashcard(self):
         response = self.client.get('/words/flashcard', follow=True)
         self.assert_successful_json(response)
