3f3035f09c18061278ac2f4796d97c3ca4c21467
Andrew Medworth <github@medworth.org.uk>
2014-08-23 21:44:08 +0800

    Set Heroku config variables in a single command

--- jianjin/heroku-configure.py
+++ jianjin/heroku-configure.py
@@ -11,8 +11,11 @@ import subprocess
 secret_key_legal_chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 r = random.SystemRandom()
 secret_key = ''.join((r.choice(secret_key_legal_chars) for i in range(50)))
-print "Setting DJANGO_SECRET_KEY..."
-subprocess.check_call(["heroku", "config:set", "DJANGO_SECRET_KEY='{0}'".format(secret_key)])
 
-# Force the Python build pack, since there are some Node configs in the root directory as well
-subprocess.check_call(["heroku", "config:set", "BUILDPACK_URL=https://github.com/heroku/heroku-buildpack-python"])
+print "Setting Heroku config variables..."
+
+# Set the secret key, and force the Python build pack, since there are some Node configs
+# in the root directory as well and this confuses Heroku
+subprocess.check_call(["heroku", "config:set", "DJANGO_SECRET_KEY='{0}'".format(secret_key),
+                       "BUILDPACK_URL=https://github.com/heroku/heroku-buildpack-python"])
+
