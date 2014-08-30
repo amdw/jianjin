c6c38e400b1531c41dcdd515b99daee378657626
Andrew Medworth <github@medworth.org.uk>
2014-08-30 11:14:38 +0800

    Disable SSLify during tests

--- words/tests.py
+++ words/tests.py
@@ -5,13 +5,14 @@ import json
 import unittest
 
 from django.test import TestCase, Client
-from django.test.utils import setup_test_environment
+from django.test.utils import setup_test_environment, override_settings
 
 import models, serializers, views
 
 USER = 'user'
 PASSWORD = 'password'
 
+@override_settings(SSLIFY_DISABLE=True)
 class LoggedInJsonTest(TestCase):
     """
     Base class for use in tests which need to be logged in and which require JSON assertions.
@@ -43,6 +44,7 @@ class LoggedInJsonTest(TestCase):
         return self.client.put(url, content_type="application/json",
                                data=json.dumps(data), follow=True)
 
+@override_settings(SSLIFY_DISABLE=True)
 class MiscJsonApiTest(LoggedInJsonTest):
     def test_get_tags(self):
         response = self.client.get('/words/tags/')
@@ -103,6 +105,7 @@ class MiscJsonApiTest(LoggedInJsonTest):
         self.assertEqual(orig_confidence, get_confidence())
 
 
+@override_settings(SSLIFY_DISABLE=True)
 class WordsApiTest(LoggedInJsonTest):
     """Test the words JSON API itself (the most complex part of the API)"""
     def __init__(self, *args, **kwargs):
@@ -343,6 +346,7 @@ class WordsApiTest(LoggedInJsonTest):
         self.assertEquals(USER, created_word.user.username)
 
 
+@override_settings(SSLIFY_DISABLE=True)
 class AuthorizationTest(LoggedInJsonTest):
     """
     Check that users cannot read or manipulate entities which don't belong to them
@@ -419,6 +423,7 @@ class AuthorizationTest(LoggedInJsonTest):
         self.assertEquals(0, len(models.Word.objects.get(pk=new_related_word['id']).related_words.all()))
 
 
+@override_settings(SSLIFY_DISABLE=True)
 class AuthenticationTest(TestCase):
     fixtures = ['testdata.json']
 
