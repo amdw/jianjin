34a4deaa531c33ba88faa9a85cfb4de6be58a73f
Andrew Medworth <github@medworth.org.uk>
2016-12-06 18:10:12 +0800

    Fix settings docstring

--- jianjin/settings.py
+++ jianjin/settings.py
@@ -1,9 +1,9 @@
-import dj_database_url
-import os
-
 """
 Django settings for jianjin project.
 """
+
+import dj_database_url
+import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
